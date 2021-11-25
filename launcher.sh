f58dadd4db5dca27f6bf6dc146201424d77d62a3
smigs <bsmigs@gmail.com>
2021-11-25 07:20:37 -0500

    minor change

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/pi/Repos/dump1090/dump1090 --aggressive --interactive --net --net-sbs-port 30003
+/home/pi/repos/adsb/dump1090/dump1090 --aggressive --interactive --net --net-sbs-port 30003
